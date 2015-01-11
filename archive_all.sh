cad8ff18838bdace4e78b83c1eb52b7078bbf84e
RouxRC <b.ooghe@gmail.com>
2015-01-11 17:50:08 +0100

    first commit

---
+++ archive_all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mkdir -p html txt
+
+domains="https://quadpad.lqdn.fr"
+for domain in $domains; do
+  cat list txt/* | grep "$domain" | sed 's/ /\n/g' | grep "^$domain/[^/]*$" | sort | uniq > list
+done
+
+for pad in `cat list`; do
+  host=$(echo $pad | sed 's|/[^/]*$||')
+  domain=$(echo $host | sed 's|https\?://||')
+  id=$(echo $pad | sed 's|^.*/\([^/]*\)$|\1|')
+  url="$host/ep/pad/export/$id/latest?format="
+  curl -sL "${url}html" > "html/${domain}_${id}.html"
+  curl -sL "${url}txt" > "txt/${domain}_${id}.txt"
+done
+
